584b781a8ff0762bdf7ef2b943c0e97df1df5a8c
HoVeRzzz <103826368+HoVeRzzz@users.noreply.github.com>
2022-08-27 18:32:25 +0100

    Add files via upload

--- Explorer-Killer.c
+++ Explorer-Killer.c
@@ -1,8 +1,3 @@
-/*
-Author: HoVeRzzz
-Purpose: Open-source script written in C with the simple purpose of providing a quick and effective way to kill and start explorer.exe
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -11,21 +6,17 @@ int main()
     char answer;
     while (answer != 'c')
     {
-        printf("type y to kill explorer.exe | type n to start explorer.exe | type c to close the program\n");
-        scanf("%c", &answer);
+        printf("type y to kill explorer.exe | type n to start explorer.exe | type close to close the program\n");
+        scanf(" %c", &answer);
         if (answer == 'y')
         {
             system("taskkill /IM explorer.exe /F");
-            printf("'Explorer killed successfully!\n");
+            printf("Explorer killed successfully!\n");
         }
-        if (answer == 'n')
+        else if (answer == 'n')
         {
             system("start explorer.exe");
             printf("Explorer started successfully!\n");
-        }
-        if (answer == 'c')
-        {
-           continue;
         }
         else
         {
@@ -33,5 +24,4 @@ int main()
         }
     }
     return 0;
-}    
-
+}
